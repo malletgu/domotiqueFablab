c81f62130f5636a63251288512c7f18a8da7d16c
rasberry_openhab <valerianbaillet@gmail.com>
2016-12-12 12:34:34 +0000

    script de configuration des variables globales gi

---
+++ scripts/conf_git.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+# Ajout de la configuration git pour permettre les push depuis le raspberry
+
+git config --global user.email "matthias.beaupere@ensimag.grenoble-inp.fr"
+git config --global user.name "Raspberry OpenHab Fablab"
+
