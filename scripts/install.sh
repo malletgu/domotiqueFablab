97b2b726f57be84a6f037c4a0a01f6449e3377eb
Valerian Baillet <valerianbaillet@gmail.com>
2016-12-12 11:33:53 +0100

    script d'install - verif du dossier d'exécution

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,20 @@
 #! /bin/sh
 
-# Script d'installation global
+# Script d'installation global de l'infrastructure OpenHab
+CURRENT_DIR="$(basename $(pwd))"
 
-#Installation de mosquitto
+if [ $CURRENT_DIR != "domotiqueFablab" ] # On vérifit que c'est bien exécuté à la racine
+then
+	echo "Veuillez exécuter ce script à la racine du git"
+	exit
+fi
+
+# Mise en place du proxy relatif au point d'accès du FabLab
+#TODO
+
+# Installation de mosquitto
 sudo -E apt-get mosquitto
 
 # Installation d'OpenHab
+
 
