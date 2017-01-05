5029557c2bf0f81577816a0ee26916615f9e7aef
Matthias Beaupère <matthias.beaupere@gmail.com>
2017-01-05 14:44:32 +0100

    ajout des script pour télécharger enocean et mqtt

--- scripts/launch_chrome.sh
+++ scripts/launch_chrome.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
-sh network.sh
+sh scripts/network.sh
 chromium-browser --proxy-server="http://www-cache.ujf-grenoble.fr:3128"
 google-chrome --proxy-server="http://www-cache.ujf-grenoble.fr:3128"
